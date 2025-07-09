1210a8e8716dd62147f1d4a460c18db3d835a1f7
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 16:31:59 -0300

    Trabalhando com variáveis e operações matemáticas

--- dia1.py
+++ dia1.py
@@ -1,3 +1,13 @@
 nome = "Marcos";
 print("Olá mundo!");
 print('Seja bem-vindo', nome);
+
+num1 = 10;
+num2 = 5;
+
+soma = num1 + num2;
+subtr = num1 - num2;
+multi = num1 * num2;
+div = num1 / num2;
+
+print(soma, subtr, multi, div);
