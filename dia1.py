02eaa26d4b4f3749d051829862edba49756ff59f
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 17:02:51 -0300

    Acrescentando comentários no código

--- dia1.py
+++ dia1.py
@@ -1,13 +1,23 @@
+# variável de string
 nome = "Marcos";
+
+# exibindo informações na tela
 print("Olá mundo!");
 print('Seja bem-vindo', nome);
 
-num1 = 10;
+# variaveis numéricas
+num1 = 11;
 num2 = 5;
 
+# soma
 soma = num1 + num2;
+# subtração
 subtr = num1 - num2;
+# multiplicação
 multi = num1 * num2;
+# divisão
 div = num1 / num2;
+# resto da divisão
+mod = num1 % num2;
 
-print(soma, subtr, multi, div);
+print(soma, subtr, multi, div, mod);
