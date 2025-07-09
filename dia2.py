2032925aa8801628f0530388f67b00d7343147bb
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-09 18:08:09 -0300

    Variáveis, Tipos de dados, Concatenação, Comparações

---
+++ dia2.py
@@ -0,0 +1,39 @@
+# Variáveis
+idade = 37
+nome = "Marcos"
+altura = 1.67
+
+# Tipos de dados
+# int, float, strings, boolean
+
+cidade = 'São Paulo'
+
+esta_logado = True
+
+print(nome)
+
+# verifica o tipo de dado
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(cidade))
+print(type(esta_logado))
+
+# Concatenação
+nomeUsuario = "João"
+frase = "Olá " + nomeUsuario + " tudo bem?"
+
+print(frase)
+
+# Conparação
+maior = 15 > 2
+print(maior)
+
+menor = 2 < 15
+print(menor)
+
+print(5 == 3)
+print("teste" == "teste")
+print("Matheus" == "matheus")
+
+print(15 != 2)
