ff62ebc8b68c09dd1f24869a92fa56d47c1483bc
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-30 12:02:21 -0300

    Operadores aritméticos no Python

---
+++ dia3.py
@@ -0,0 +1,12 @@
+# operadores aritméticos
+
+a = 5
+b = 2
+
+print(a + b) # soma
+print(a - b) # divisão
+print(a * b) # multiplicação
+print(a / b) # divisao
+print(a // b) # divisão inteira
+print(a % b) # resto da divisao
+print(a ** b) # exponenciação
