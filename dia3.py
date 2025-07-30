9883aa4b4c32999ce551c16b46ee4a7602bbbf4c
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-07-30 12:07:42 -0300

    Operadores de comparação no Python

--- dia3.py
+++ dia3.py
@@ -10,3 +10,15 @@ print(a / b) # divisao
 print(a // b) # divisão inteira
 print(a % b) # resto da divisao
 print(a ** b) # exponenciação
+
+
+# Operadores de Comparação
+
+x = 10
+y = 5
+
+print(x > 5)
+print(x >= 5)
+print(y < 5)
+print(y <= 5)
+print(x != y)
